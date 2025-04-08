dbba56c0bedfd97c3a6ec9717192404ea451a985
Naufal <ahmadnaufalalghifari@gmail.com>
2025-04-08 07:32:25 +0700

    inialisasi

---
+++ Modul7_Kelompok3/Program.cs
@@ -0,0 +1,13 @@
+﻿using Modul7_Kelompok3;
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+
+        DataMahasiswa103022300006.ReadJSON();
+        Console.WriteLine();
+
+    }
+}
