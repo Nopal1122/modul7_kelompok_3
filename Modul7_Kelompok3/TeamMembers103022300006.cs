47acdd4bef9846482d91cbfdb5dea49e84c42cb1
Naufal <ahmadnaufalalghifari@gmail.com>
2025-04-08 08:08:37 +0700

    menambahkan class members1030223000006

---
+++ Modul7_Kelompok3/TeamMembers103022300006.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using static Modul7_Kelompok3.DataMahasiswa103022300006;
+
+
+namespace Modul7_Kelompok3
+{
+    class TeamMembers103022300006
+    {
+
+        public class members
+        {
+            public string depan { get; set; }
+            public string belakang { get; set; }
+            public string gender { get; set; }
+            public long age { get; set; }
+            public string nim { get; set; }
+
+
+        }
+        public class Members
+        {
+            public List<members> members { get; set; }
+
+
+        }
+
+        public static void ReadJSON()
+        {
+            string path = "jurnal7_2_103022300006.json";
+            string JsonSring = File.ReadAllText(path);
+
+            Members mem = JsonSerializer.Deserialize<Members>(JsonSring);
+
+            foreach (var members in )
+            {
+                Console.WriteLine($"kode matkul {matkul.code}nama matkul: {matkul.name}");
+            }
+        }
+    }
+}
+
+    }
+}
--- Modul7_Kelompok3/obj/Debug/net8.0/Modul7_Kelompok3.AssemblyInfo.cs
+++ Modul7_Kelompok3/obj/Debug/net8.0/Modul7_Kelompok3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul7_Kelompok3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbba56c0bedfd97c3a6ec9717192404ea451a985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41ed671b2fda8342326f2a2ce48e5ea05bf2944")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul7_Kelompok3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul7_Kelompok3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
